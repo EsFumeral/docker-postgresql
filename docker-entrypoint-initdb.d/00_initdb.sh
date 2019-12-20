a852be5b64aac4f1b206d9b72c96173c247c2913
gdeignacio <gdeignacio@gmail.com>
2019-12-20 18:41:19 +0100

    Initdb

--- docker-entrypoint-initdb.d/00_initdb.sh
+++ docker-entrypoint-initdb.d/00_initdb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source /docker-entrypoint-initdb.d/scripts/00-setenv.sh
-source /docker-entrypoint-initdb.d/scripts/11-create-tablespace-folder.sh
-source /docker-entrypoint-initdb.d/scripts/21-create-database.sh
-source /docker-entrypoint-initdb.d/scripts/22-create-schema.sh
+#source /docker-entrypoint-initdb.d/scripts/11-create-tablespace-folder.sh
+#source /docker-entrypoint-initdb.d/scripts/21-create-database.sh
+#source /docker-entrypoint-initdb.d/scripts/22-create-schema.sh
 
 cd /
