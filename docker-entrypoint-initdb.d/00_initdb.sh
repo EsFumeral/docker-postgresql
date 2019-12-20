d27336f9ecb437ca893d7ef115793d98e1164f60
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-12-20 13:37:13 +0100

    Scripts

--- docker-entrypoint-initdb.d/00_initdb.sh
+++ docker-entrypoint-initdb.d/00_initdb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /docker-entrypoint-initdb.d/initdb.d/00-setenv.sh
-source /docker-entrypoint-initdb.d/initdb.d/11-create-tablespace-folder.sh
-source /docker-entrypoint-initdb.d/initdb.d/21-create-database.sh
-source /docker-entrypoint-initdb.d/initdb.d/22-create-schema.sh
+source /docker-entrypoint-initdb.d/scripts/00-setenv.sh
+source /docker-entrypoint-initdb.d/scripts/11-create-tablespace-folder.sh
+source /docker-entrypoint-initdb.d/scripts/21-create-database.sh
+source /docker-entrypoint-initdb.d/scripts/22-create-schema.sh
 
 cd /
