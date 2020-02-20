be7310e536999b4ce1b7a30af03fe29749625920
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2020-02-20 16:12:33 +0100

    Quoted vars

--- docker-entrypoint-initdb.d/sql/create_database dummy.sql
+++ docker-entrypoint-initdb.d/sql/create_database dummy.sql
@@ -1,12 +1,13 @@
 
 CREATE DATABASE DUMMY
-WITH OWNER = postgres
-        ENCODING = 'UTF8'
-        TABLESPACE = pg_default
-        LC_COLLATE = 'en_US.utf8'
-        LC_CTYPE = 'en_US.utf8'
-        CONNECTION LIMIT = -1;
+    WITH OWNER = postgres
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
 
+/*
 ALTER DATABASE DUMMY
  SET client_encoding ='UTF8';
 
@@ -25,3 +26,4 @@ LOCATION '/var/lib/postgresql/data/tablespaces';
 ALTER ROLE DUMMY SET default_tablespace TO DUMMY;
 
 ALTER DATABASE DUMMY OWNER to DUMMY;
+*/
