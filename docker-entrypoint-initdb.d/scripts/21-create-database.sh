6decdb76bb660944503ef4786bc57a97132a7fdc
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2020-02-21 14:49:44 +0100

    Filename

--- docker-entrypoint-initdb.d/scripts/21-create-database.sh
+++ docker-entrypoint-initdb.d/scripts/21-create-database.sh
@@ -7,13 +7,13 @@ psql \
     --username "$POSTGRES_USER" \
     --dbname "$POSTGRES_DB" \
     --file /docker-entrypoint-initdb.d/sql/create_database_dummy.sql \
-    --echo-all #\
-    #-v AUTOCOMMIT=off \
-    #-v APP_DATABASE_NAME=$APP_DATABASE_NAME \
-    #-v APP_USER_NAME=$APP_USER_NAME \
-    #-v QAPP_WWW_USER_NAME=\'$APP_WWW_USER_NAME\' \
-    #-v QAPP_TABLESPACES=\'$APP_TABLESPACES\' \
-    #-v QLC_NAME=\'$LC_NAME\'
+    --echo-all \
+    -v AUTOCOMMIT=off \
+    -v APP_DATABASE_NAME=$APP_DATABASE_NAME \
+    -v APP_USER_NAME=$APP_USER_NAME \
+    -v QAPP_WWW_USER_NAME=\'$APP_WWW_USER_NAME\' \
+    -v QAPP_TABLESPACES=\'$APP_TABLESPACES\' \
+    -v QLC_NAME=\'$LC_NAME\'
     
 #psql_exit_status = $?
 
